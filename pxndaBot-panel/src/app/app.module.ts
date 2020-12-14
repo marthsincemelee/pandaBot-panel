b68f3fa6d0ed8c1064f4038381d8130143a22255
Finn <f.markwitz@ssc-servces.de>
2020-12-14 19:41:37 +0100

    added ng-zorro for ui

--- pxndaBot-panel/src/app/app.module.ts
+++ pxndaBot-panel/src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { de_DE } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import de from '@angular/common/locales/de';
+
+registerLocaleData(de);
 
 @NgModule({
   declarations: [
@@ -10,9 +19,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: de_DE }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
