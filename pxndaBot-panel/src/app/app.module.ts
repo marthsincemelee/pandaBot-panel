273e97687e522d7bd459ace79a3029a1fd0e1cbf
Finn <f.markwitz@ssc-servces.de>
2020-12-14 22:51:43 +0100

    added basic view

--- pxndaBot-panel/src/app/app.module.ts
+++ pxndaBot-panel/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ResultListComponent } from './result-list/result-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,21 +13,35 @@ import { registerLocaleData } from '@angular/common';
 import de from '@angular/common/locales/de';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { YoutubeWindowComponent } from './youtube-window/youtube-window.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 
 registerLocaleData(de);
 
 @NgModule({
   declarations: [
     AppComponent,
-    YoutubeWindowComponent
+    YoutubeWindowComponent,
+    ResultListComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    NzCardModule,
+    NzButtonModule,
+    NzIconModule,
     HttpClientModule,
     NzLayoutModule,
-    BrowserAnimationsModule
+    NzInputModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [{ provide: NZ_I18N, useValue: de_DE }],
   bootstrap: [AppComponent]
